60a9eaa9249d24738bec60932a8a8ebf470401fd
samidna <samidna@code.edu.az>
2024-03-07 18:31:11 +0400

    created new project

---
+++ Method_Overload.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public static  class Method_Overload
+    {
+        public static int MaxNum(int a,int b,int c)
+        {
+            int max;
+            if(a>b) max = a;
+            else if(b>c) max = b;
+            else max=c;
+            return max;
+        }
+        public static int MaxNum(int a,int b,int c,int d)
+        {
+            int max = MaxNum(a, b, c);
+            if (d>max) return d;
+            else return max;
+        }
+
+        public static int MaxNum(int a, int b, int c, int d,int e)
+        {
+            int max = MaxNum(a, b, c,d);
+            if (e>max) return e;
+            else return max;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using ConsoleApp4;
+
+Console.WriteLine(Method_Overload.MaxNum(3, 8, 7));
+Console.WriteLine(Method_Overload.MaxNum(3, 8, 7,9));
+Console.WriteLine(Method_Overload.MaxNum(3, 8, 7,16));
